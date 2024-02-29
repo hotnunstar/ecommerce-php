554721de955ee3327f9b191da242de37768b5d2c
Nuno <nu-nos@hotmail.com>
2024-02-29 22:44:57 +0000

    Login Page

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,6 +7,8 @@ class Page{
     private $tpl;
     private $options = [];
     private $defaults = [
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -21,7 +23,7 @@ class Page{
 
         $this->tpl = new Tpl;
         $this->setData($this->options["data"]);
-        $this->tpl->draw("header");
+        if ($this->options["header"] == true) $this->tpl->draw("header");
     }
 
     private function setData($data = array()) {
@@ -36,7 +38,7 @@ class Page{
     }
 
     public function __destruct() {
-        $this->tpl->draw("footer");
+        if ($this->options["footer"] == true) $this->tpl->draw("footer");
     }
 }
 
