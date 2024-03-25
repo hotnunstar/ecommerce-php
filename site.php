2ef6593ffc712b960f512e3767b5491ccd6bed4f
Nuno <nu-nos@hotmail.com>
2024-03-25 19:38:42 +0000

    Products card

--- site.php
+++ site.php
@@ -49,7 +49,37 @@ $app->get("/products/:desurl", function($desurl){
 $app->get("/cart", function() {
 	$cart = Cart::getFromSession();
 	$page = new Page();
-	$page->setTpl("cart");
+	$page->setTpl("cart", [
+		'cart' => $cart->getValues(),
+		'products' => $cart->getProducts()
+	]);
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct) {
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+
+	header("Location: /cart");
+	exit;
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct) {
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$cart->removeProduct($product);
+	header("Location: /cart");
+	exit;
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct) {
+	$product = new Product();
+	$product->get((int)$idproduct);
+	$cart = Cart::getFromSession();
+	$cart->removeProduct($product, true);
+	header("Location: /cart");
+	exit;
 });
 
 ?>
